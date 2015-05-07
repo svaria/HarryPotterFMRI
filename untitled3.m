1a4b3c675aceb51b930e67e796435dc04cdeafb8
svaria <shaanvaria@MacBook-Air.local>
2015-05-07 17:24:56 -0400

    mse

---
+++ untitled3.m
@@ -0,0 +1,40 @@
+dats = cell(4,1);
+dats{1}= s1dat1;
+dats{2} = s1dat2;
+dats{3} = s1dat3;
+dats{4} = s1dat4;
+fs = cell(4,1);
+fs{1} = feat1;
+fs{2} = feat2;
+fs{3} = feat3;
+fs{4} = feat4;
+size(dats{1},2)
+randperm(size(dats{1},2)-20,1)
+
+for i = 1:100,
+    a = randperm(4,2);
+    starts = cell(2,2);
+    starts{1,1} = randperm(size(dats{a(1)},2)-20,1);
+    starts{1,2} = a(1);
+    starts{2} = randperm(size(dats{a(2)},2)-20,1);
+    starts{2,2} = a(2);
+    inp_passage = (rand > 0.5)+1;
+    story = starts{inp_passage,2};
+    sIndex = starts{inp_passage,1};
+
+    X = tonndata(fs{story}(:,sIndex:sIndex+20),true,false);
+    T = tonndata(dats{story}(:,sIndex:sIndex+20),true,false);
+    [x,xi,ai,t] = preparets(net,X,T);
+    predicted_out = net(x,xi,ai);
+
+    err1 = []
+    err2 = []
+    for j=1:19,
+        dats{starts{1,2}}(:,starts{1,1}:starts{1,1}+20);
+        dats{starts{2,2}}(:,starts{2,1}:starts{2,1}+20);
+        err1 = [err1, predicted_out{j}]
+    end
+    
+    
+end
+    
